e584c56692f734d55df9b8ae51b3ba663efbca90
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-10 15:40:44 +0500

    modified index.tsx file by adding links and creditcardicon

--- resources/js/Cart/Index.tsx
+++ resources/js/Cart/Index.tsx
@@ -1,6 +1,6 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 function Index(  {
     csrf_token,
     cartItems,
@@ -14,7 +14,35 @@ function Index(  {
             <div className="container mx-auto p-8 flex flex-col lg:flex-row gap-4">
                 <div className="card flex-1 bg-white dark:bg-gray-800 order-2 lg:order-1">
                     <div className="card-body">
-
+                        <h2 className="text-lg font-bold">
+                            Shopping Cart
+                        </h2>
+                        <div className="my-4">
+                            {Object.keys(cartItems).length === 0 && (
+                                <div className="py-2 text-gray-500 text-500">
+                                    You don't have any items yet
+                                </div>
+                            )}
+                            {Object.values(cartItems).map((cartItem: any) => (
+    <div key={cartItem.user.id}>
+        <div className="flex items-center justify-between pb-4 border-b border-white mb-4">
+            <Link href="/" className="underline">
+                {cartItem.user.name}
+            </Link>
+            <div>
+                <form action={route('cart.checkout')} method="post">
+                    <input type="hidden" name="_token" value={csrf_token}/>
+                    <input type="hidden" name="vendor_id" value={cartItem.user.id}/>
+                    <button className="btn btn-sm btn-ghost">
+                        <CreditCardIcon className={"size-6"}/>
+                                Pay only for this seller
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+))}
+                        </div>
                     </div>
                 </div>
                 <div className="card bg-white dark:bg-gray-800 lg:min-w-[260px] order-1 lg:order-2">
