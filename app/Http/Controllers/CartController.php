5320a9a7dfd74644a30fe3a913b9d4671c7a6438
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-18 02:52:34 +0500

    updated cartcontroller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,6 +7,11 @@ use App\Services\CartService;
 use App\Models\Product;
 use Inertia\Inertia;
 use App\Models\CartItem;
+use Illuminate\Support\Facades\DB;
+use App\Models\Order;
+use App\OrderStatusEnum;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\Log;
 
 class CartController extends Controller
 {
@@ -104,7 +109,83 @@ class CartController extends Controller
         return back()->with('success', 'Product was removed from cart');
     }
 
-    public function checkout() {
-        
+    public function checkout(Request $request, CartService $cartService) {
+        \Stripe\Stripe::setApiKey(config('app.stripe_secret_key'));
+
+        $vendorId = $request->input('vendor_id');
+
+        $allCartItems = $cartService->getCartItemsGrouped();
+
+        DB::beginTransaction();
+
+        try {
+            $checkOutCartItems = $allCartItems;
+            if ($vendorId) {
+                $checkOutCartItems = [$allCartItems[$vendorId]];
+            }
+            $orders = [];
+            $lineItems = [];
+            foreach ($checkOutCartItems as $item) {
+                $user = $item['user'];
+                $cartItems = $item['items'];
+
+                $order = Order::create([
+                    'stripe_session_id' => null,
+                    'user_id' => $request->user()->id,
+                    'vendor_user_id' => $user['id'],
+                    'total_price' => $item['total_price'],
+                    'status' => OrderStatusEnum::Draft->value
+                ]);
+                $orders[] = $order;
+
+                foreach ($cartItems as $cartItem) {
+                    OrderItem::create([
+                        'order_id' => $order->id,
+                        'product_id' => $cartItem['product_id'],
+                        'quantity' => $cartItem['quantity'],
+                        'price' => $cartItem['price'],
+                        'variation_type_options_ids' => $cartItem['option_ids']
+                    ]);
+                    $description = collect($cartItem['options'])->map(function ($item) {
+                        return "{$item['type']['name']}: {$item['name']}";
+                    })->implode(', ');
+
+                    $lineItem = [
+                        'price_data' => [
+                            'currency' => config('app.currency'),
+                            'product_data' => [
+                                'name' => $cartItem['title'],
+                                'images' => [$cartItem['image']]
+                            ],
+                            'unit_amount' => $cartItem['price'] * 100
+                        ],
+                        'quantity' => $cartItem['quantity']
+                    ];
+                    if ($description) {
+                        $lineItem['price_data']['product_data']['description'] = $description;
+                    }
+                    $lineItems[] = $lineItem;
+                }
+            }
+            $session = \Stripe\Checkout\Session::create([
+                'customer_email' => $request->user()->email,
+                'line_items' => $lineItems,
+                'mode' => 'payment',
+                'success_url' => route('stripe.success', []) . "?session_id={CHECKOUT_SESSION_ID}",
+                'cancel_url' => route('stripe.failure', [])
+            ]);
+
+            foreach ($orders as $order) {
+                $order->stripe_session_id = $session->id;
+                $order->save();
+            }
+
+            DB::commit();
+            return redirect($session->url);
+        } catch (\Exception $e) {
+            Log::error($e);
+            DB::rollBack();
+            return back()->with('error', $e->getMessage() ?: 'Something went wrong');
+        }
     }
 }
