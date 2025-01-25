8fead6ebbeb60db253f70ac314b6c19ddad711a1
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-26 03:55:56 +0500

    solved stripe bug

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\Order;
+use App\OrderStatusEnum;
+use App\Models\CartItem;
+use App\Models\OrderItem;
 
 class StripeController extends Controller
 {
@@ -17,9 +20,9 @@ class StripeController extends Controller
     }
 
     public function webhook(Request $request) {
-        $stripe = new \Stripe\StripeClient(config('app.stripe.secret_key'));
+        $stripe = new \Stripe\StripeClient(config('app.stripe_secret_key'));
 
-        $endpoint_secret = config('app.stripe.endpoint_secret');
+        $endpoint_secret = config('app.stripe_webhook_secret');
         $payload = $request->getContent();
         $sig_header = request()->header('Stripe-Signature');
         $event = null;
@@ -64,11 +67,60 @@ class StripeController extends Controller
 
                 foreach ($orders as $order) {
                     $vendorShare = $order->total_price / $totalAmount;
+                    /** @var Order $order */
+                    $order->online_payment_commission = $vendorShare * $stripeFee;
+                    $order->website_commission = ($order->total_price - $order->online_payment_commission) / 100 * $platformFeePercent;
+                    $order->vendor_subtotal = $order->total_price - $order->online_payment_commission - $order->website_commission;
 
-                    $order->online_payment_commission = $vendorShare;
+                    $order->save();
                 }
-            case 'checkout.session.completed':
+            break;
 
+            case 'checkout.session.completed':
+                $session = $event->data->object;
+                $pi = $session['payment_intent'];
+
+                $orders = Order::query()->with(['orderItems'])->where(['stripe_session_id' =>
+            $session['id']])->get();
+
+            $productsToDeletedFromCart = [];
+            foreach ($orders as $order) {
+                $order->payment_intent = $pi;
+                $order->status = OrderStatusEnum::Paid;
+                $order->save();
+
+                $productsToDeletedFromCart = [
+                    ...$productsToDeletedFromCart,
+                    ...$order->orderItems->map(fn($item) => $item->product_id)->toArray()
+                ];
+
+                foreach ($order->orderItems as $orderItem){
+                    /** @var OrderItem $orderItem */
+                    $options = $orderItem->variation_type_options_ids;
+                    $product = $orderItem->product;
+                    if ($options) {
+                        sort($options);
+                        $variation = $product->variations()->where('variation_type_options_ids', $options)
+                        ->first();
+
+                        if ($variation && $variation->quantity != null) {
+                            $variation->quantity = $orderItem->quantity;
+                            $variation->save();
+                        }
+                    } else if ($product->quantity != null) {
+                        $product->quantity = $orderItem->quantity;
+                        $product->save();
+                    }
+                }
+            }
+            CartItem::query()->where('user_id', $order->user_id)
+            ->whereIn('product_id', $productsToDeletedFromCart)
+            ->where('saved_for_later', false)->delete();
+            break;
+            default:
+                echo "Received unknown event type " . $event->type;
         }
-    }
+        return response('', 200);
+        }
+
 }
