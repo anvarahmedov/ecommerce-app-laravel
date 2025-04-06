1d74eea4539f5858696393dd683893524a3a7058
anvarahmedov <boyoftokyo19@gmail.com>
2025-04-07 00:56:18 +0500

    updated vendor details

--- resources/js/Pages/Profile/Partials/VendorDetails.tsx
+++ resources/js/Pages/Profile/Partials/VendorDetails.tsx
@@ -6,6 +6,7 @@ import Modal from '@/Components/Modal';
 import InputLabel from '@/Components/InputLabel';
 import TextInput from '@/Components/TextInput';
 import InputError from '@/Components/InputError';
+import { Textarea } from '@headlessui/react';
 
 export default function VendorDetails(
     props: {
@@ -70,6 +71,82 @@ export default function VendorDetails(
 
     return (
         <section className={props.className}>
+            {recentlySuccessful && <div className='toast toast-top toast-end'>
+            <div>
+                    <span>{successMessage}</span>
+            </div>
+            </div>}
+
+            <header>
+                <h2 className="flex justify-between mb-8 text-lg font-medium text-gray-900 dark:text-gray-100">
+                    Vendor Details
+                    {user.vendor?.status === 'pending' && <span className="badge badge-warning">user.vendor.status_label</span>}
+                    {user.vendor?.status === 'approved' && <span className="badge badge-success">user.vendor.status_label</span>}
+                    {user.vendor?.status === 'rejected' && <span className="badge badge-error">user.vendor.status_label</span>}
+                </h2>
+
+
+            </header>
+
+            <div>
+                {!user.vendor && <PrimaryButton onClick={() => setShowBecomeVendorConfirmation(true)} disabled={processing}>
+                    Become a vendor
+            </PrimaryButton>
+                }
+
+                {user.vendor && (
+                    <>
+                    <form onSubmit={updateVendor}>
+                        <div className='mb-4'>
+                            <InputLabel htmlFor="name" value="Store Name" />
+
+                            <TextInput
+                                id="name"
+                                className='mt-1 block w-full'
+                                value={data.store_name}
+                                onChange={onStoreNameChange}
+                                required
+                                isFocused={true}
+                                autoComplete="name"
+                            />
+                            <InputError message={errors.store_name} className='mt-2'/>
+                        </div>
+
+                        <div className='mb-4'>
+                            <InputLabel htmlFor="address" value="Store Address"/>
+
+                            <textarea
+                                className="mt-1 block w-full"
+                                value={data.store_address}
+                                onChange={(e) => setData('store_address', e.target.value) }
+                                placeholder="Enter your store address"
+                                >
+                            </textarea>
+                            <InputError message={errors.store_address} className='mt-2'/>
+                        </div>
+                        <div className='flex items-center gap-4'>
+                            <PrimaryButton disabled={processing}>
+                                Update
+                            </PrimaryButton>
+                        </div>
+                    </form>
+
+                    <form action={route('stripe.connect')}
+                    method={'post'}
+                    className='{my-8}'>
+                        <input type = "hidden" name="_token" value={token}/>
+                        {user.stripe_account_active && (<div className={'text-center text-gray-600 my-4 text-sm'}>
+                            You are successfully connected to Stripe.
+                        </div>)}
+
+                        <button className='btn btn-primary w-full' disabled={user.stripe_account_active}>
+                            Connect to Stripe
+                        </button>
+                    </form>
+                    </>
+                )}
+
+            </div>
             </section>
     );
 }
